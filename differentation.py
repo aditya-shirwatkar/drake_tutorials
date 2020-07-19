a8720016283bbc4196b003e6579d3c1e4c2373a3
aditya-shirwatkar <cosmosaditya02@gmail.com>
2020-07-19 22:04:42 +0530

    update

--- differentation.py
+++ differentation.py
@@ -18,6 +18,10 @@ subject to x(0) + x(1) = 1
 """
 
 from pydrake.all import eq, MathematicalProgram, Solve, Variable
+# from pydrake.solvers.snopt import SnoptSolver
+from pydrake.solvers.ipopt import IpoptSolver
+from matplotlib import pyplot as plt
+
 from pydrake.common.containers import namedview
 from pydrake.autodiffutils import AutoDiffXd
 import numpy as np
@@ -36,10 +40,9 @@ class Model(MathematicalProgram):
 	def __init__(self):
 		super().__init__()
 
-		self.N = 40; self.T = 1.
+		self.N = 10; self.T = 1.
 		self.num_steps = 2
 		self.step_max = 0.25; self.tauMax = 1.5
-		self.pi = np.pi; 
 		self.l = np.array([0.25,0.25,0.25,0.25,0.25])
 		self.m = np.array([0.25,0.25,0.25,0.25,0.25])
 		self.i = self.m * (self.l**2) /12
@@ -57,9 +60,13 @@ class Model(MathematicalProgram):
 
 		self.CreateVariables()
 
+		# self.getKinematics()
+
 		# self.ComputeTransformations()
 
 		self.SetConstraints()
+
+		# self.Visualize()
 
 		# # Set up the optimization problem.
 		# self.state = self.prog.NewContinuousVariables(14, 'state')
@@ -72,64 +79,106 @@ class Model(MathematicalProgram):
 		self.u = self.prog.NewContinuousVariables(4, self.N,'u')
 		self.dx = self.prog.NewContinuousVariables(5, self.N, 'dx')
 		self.x = self.prog.NewContinuousVariables(5, self.N, 'x')
-		# self.left_foot= self.prog.NewContinuousVariables(2, self.N, 'left_tip')
+		self.l_force= self.prog.NewContinuousVariables(2, self.N, 'left_force')
+		self.r_force= self.prog.NewContinuousVariables(2, self.N, 'right_force')
 		# self.foot_contacts = self.prog.NewBinaryVariables(2, self.N, 'foot_contacts')
 		# self.right_foot_contact = self.prog.NewBinaryVariables(1, self.N, 'right_contact')
 
-	def ComputeTransformations(self):
-		self.world_frame = np.array([0, 0]).reshape(2, 1)
-		self.T01 = []; self.T12 = []; self.T23 = []; self.T24 = []; self.T45 = []
+	def getKinematics(self):
+		self.p1 = [];self.p2 = [];self.p3 = [];self.p4 = [];self.p5 = []
 		for n in range(self.N):
-			s1, c1 = np.sin(self.x[0, n]), np.cos(self.x[0, n]) 
-			s2, c2 = np.sin(self.x[1, n]), np.cos(self.x[1, n]) 
-			s3, c3 = np.sin(self.x[2, n]), np.cos(self.x[2, n]) 
-			s4, c4 = np.sin(self.x[3, n]), np.cos(self.x[3, n]) 
-			s5, c5 = np.sin(self.x[4, n]), np.cos(self.x[4, n]) 
-			
-			self.T01.append(np.array([
-				[c1, s1, self.left_foot[0, n]],
-				[-s1, c1, self.left_foot[1, n]],
-				[0, 0, 1]
-			]))
-			self.T12.append( self.T01[-1] @ (np.array(
-											[[c2, s2, self.l[0]],
-											[-s2, c2, self.l[0]],
-											[0, 0, 1]]
-											))
-							)
-			self.T23.append( self.T12[-1] @ (np.array(
-											[[c3, s3, self.l[1]],
-											[-s3, c3, self.l[1]],
-											[0, 0, 1]]
-											))
-							)
-			self.T24.append( self.T12[-1] @ (np.array(
-											[[c4, s4, self.l[0]],
-											[-s4, c4, self.l[0]],
-											[0, 0, 1]]
-											))
-							)
-			# print(self.T01)
+			s1, c1 = pymath.sin(self.x[0, n]), pymath.cos(self.x[0, n]) 
+			s2, c2 = pymath.sin(self.x[1, n]), pymath.cos(self.x[1, n]) 
+			s3, c3 = pymath.sin(self.x[2, n]), pymath.cos(self.x[2, n]) 
+			s4, c4 = pymath.sin(self.x[3, n]), pymath.cos(self.x[3, n]) 
+			s5, c5 = pymath.sin(self.x[4, n]), pymath.cos(self.x[4, n]) 
+
+			self.p1.append([self.l[0]*s1, self.l[0]*c1])
+			self.p2.append([self.l[1]*s2+self.p1[-1][0], self.l[1]*c2+self.p1[-1][1]])
+			self.p3.append([self.l[2]*s3+self.p2[-1][0], self.l[2]*c3+self.p2[-1][1]])
+			self.p4.append([self.l[3]*s4+self.p3[-1][0], self.l[3]*c4+self.p3[-1][1]])
+			self.p5.append([self.l[4]*s5+self.p4[-1][0], self.l[4]*c5+self.p4[-1][1]])
 
 	def SetConstraints(self):
 
-		def collocation(z):
+		h = self.h
+
+		def collocation_dot(z):
 			ceq = []
-			q = [z[0], z[1], z[2], z[3], z[4]]
-			dq = [z[5], z[6], z[7], z[8], z[9]]
-			u = [z[10], z[11], z[12], z[13]]
-			ddq = self.ComputeManipulators(q, dq, u)
-			# print(ddq.shape)
-			ceq.extend([(dq[i] - ddq[i]) for i in range(len(q))])
-			# print(ceq)
-			# ceq.extend([(ddq[i] - dq[i]) for i in range(len(q))])
+			q1 = [z[0], z[1], z[2], z[3], z[4]]
+			dq1 = [z[5], z[6], z[7], z[8], z[9]]
+			u1 = [z[10], z[11], z[12], z[13]]
+			ddq1 = self.ComputeManipulators(q1, dq1, u1)
+
+			q2 = [z[0+14], z[1+14], z[2+14], z[3+14], z[4+14]]
+			dq2 = [z[5+14], z[6+14], z[7+14], z[8+14], z[9+14]]
+			u2 = [z[10+14], z[11+14], z[12+14], z[13+14]]
+			ddq2 = self.ComputeManipulators(q2, dq2, u2)
+			
+			ceq.extend([((ddq2[i] - ddq1[i]) - (h*(dq2[i] - dq1[i]))) for i in range(len(dq1))])
+			# ceq.extend([((dq2[i] - dq1[i]) - (h*(q2[i] - q1[i]))) for i in range(len(dq1))])
+			
+			# ceq.extend([(u[i] - q[i]) for i in range(len(u))])
+			# print(len(ceq))
+			
 			return np.array(ceq)
 
-		for n in range(self.N):
+		def collocation(z):
+			ceq = []
+			q1 = [z[0], z[1], z[2], z[3], z[4]]
+			dq1 = [z[5], z[6], z[7], z[8], z[9]]
+			u1 = [z[10], z[11], z[12], z[13]]
+			ddq1 = self.ComputeManipulators(q1, dq1, u1)
+
+			q2 = [z[0+14], z[1+14], z[2+14], z[3+14], z[4+14]]
+			dq2 = [z[5+14], z[6+14], z[7+14], z[8+14], z[9+14]]
+			u2 = [z[10+14], z[11+14], z[12+14], z[13+14]]
+			ddq2 = self.ComputeManipulators(q2, dq2, u2)
+			
+			# ceq.extend([((ddq2[i] - ddq1[i]) - (h*(dq2[i] - dq1[i]))) for i in range(len(dq1))])
+			ceq.extend([((dq2[i] - dq1[i]) - (h*(q2[i] - q1[i]))) for i in range(len(dq1))])
+			
+			# ceq.extend([(u[i] - q[i]) for i in range(len(u))])
+			# print(len(ceq))
+			
+			return np.array(ceq)
+
+		def positional(z):
+			p1, p2, p3, p4, p5 = self.ConstraintKinematics(z)
+			return np.array([p5[0], p5[1]])
+
+		x0 = [-0.6,0.7,0.0,-0.5,-0.3]
+		self.prog.AddBoundingBoxConstraint(x0, x0, self.x[:,0])
+		# self.prog.AddConstraint(positional, lb=[-3*self.step_max, 0], ub=[0, 0],
+		# 					vars=[self.x[0,0], self.x[1,0], self.x[2,0], self.x[3,0], self.x[4,0]])
+
+		for n in range(self.N - 1):
+			self.prog.AddConstraint(collocation_dot, lb=np.array([0]*5), ub=np.array([0]*5), 
+							vars=[self.x[0,n], self.x[1,n], self.x[2,n], self.x[3,n], self.x[4,n], 
+								self.dx[0,n], self.dx[1,n],self.dx[2,n],self.dx[3,n],self.dx[4,n],  
+								self.u[0,n], self.u[1,n], self.u[2,n], self.u[3,n],
+								self.x[0,n+1], self.x[1,n+1], self.x[2,n+1], self.x[3,n+1], self.x[4,n+1], 
+								self.dx[0,n+1], self.dx[1,n+1],self.dx[2,n+1],self.dx[3,n+1],self.dx[4,n+1],  
+								self.u[0,n+1], self.u[1,n+1], self.u[2,n+1], self.u[3,n+1]])
+
 			self.prog.AddConstraint(collocation, lb=np.array([0]*5), ub=np.array([0]*5), 
 							vars=[self.x[0,n], self.x[1,n], self.x[2,n], self.x[3,n], self.x[4,n], 
 								self.dx[0,n], self.dx[1,n],self.dx[2,n],self.dx[3,n],self.dx[4,n],  
-								self.u[0,n], self.u[1,n], self.u[2,n], self.u[3,n]])
+								self.u[0,n], self.u[1,n], self.u[2,n], self.u[3,n],
+								self.x[0,n+1], self.x[1,n+1], self.x[2,n+1], self.x[3,n+1], self.x[4,n+1], 
+								self.dx[0,n+1], self.dx[1,n+1],self.dx[2,n+1],self.dx[3,n+1],self.dx[4,n+1],  
+								self.u[0,n+1], self.u[1,n+1], self.u[2,n+1], self.u[3,n+1]])
+
+			self.prog.AddCost(sum(self.x[:,n]**2) + sum(self.dx[:,n]**2) + sum(self.u[:,n]**2))
+
+			self.prog.AddBoundingBoxConstraint([-self.tauMax]*4, [self.tauMax]*4, self.u[:,n])
+			self.prog.AddBoundingBoxConstraint([-np.pi]*5, [np.pi]*5, self.x[:,n])
+			self.prog.AddBoundingBoxConstraint([-np.pi]*5, [np.pi]*5, self.dx[:,n])
+		
+		self.prog.AddConstraint(positional, lb=[self.step_max, 0], ub=[self.step_max, 0],
+							vars=[self.x[0,-1], self.x[1,-1], self.x[2,-1], self.x[3,-1], self.x[4,-1]])
+
+		# self.prog.AddBoundingBoxConstraint(x0[::-1], x0[::-1], self.x[:,-1])
 
 	def ComputeManipulators(self, q, dq, u):
 		n = np.array([
@@ -174,11 +223,158 @@ class Model(MathematicalProgram):
 		ddq = pymath.inv(M) @ (T - G - (C @ (np.array(dq).reshape(5, 1)**2)))
 		return ddq
 
+	def ConstraintKinematics(self, x):
+		p1 = [];p2 = [];p3 = [];p4 = [];p5 = []
+		s1, c1 = pymath.sin(x[0]), pymath.cos(x[0]) 
+		s2, c2 = pymath.sin(x[1]), pymath.cos(x[1]) 
+		s3, c3 = pymath.sin(x[2]), pymath.cos(x[2]) 
+		s4, c4 = pymath.sin(x[3]), pymath.cos(x[3]) 
+		s5, c5 = pymath.sin(x[4]), pymath.cos(x[4]) 
+
+		p1 = [self.l[0]*s1, self.l[0]*c1]
+		p2 = [self.l[1]*s2+p1[0], self.l[1]*c2+p1[1]]
+		p3 = [self.l[2]*s3+p2[0], self.l[2]*c3+p2[1]]
+		p4 = [self.l[3]*s4+p3[0], self.l[3]*c4+p3[1]]
+		p5 = [self.l[4]*s5+p4[0], self.l[4]*c5+p4[1]]
+
+		return p1, p2, p3, p4, p5
 f = Model()
+
 result = Solve(f.prog)
+
 assert(result.is_success()), "Optimization failed"
 
-print('u = ', f.u.shape, 'x = ', f.x.shape, 'dx = ', f.dx.shape)
+print("Success? ", result.is_success())
+# Print the solution to the decision variables.
+# print('x* = ', result.GetSolution(x))
+# Print the optimal cost.
+print('optimal cost = ', result.get_optimal_cost())
+# Print the name of the solver that was called.
+print('solver is: ', result.get_solver_id().name())
+
+time = np.linspace(0, f.T, f.N)
+x_sol = result.GetSolution(f.x)
+dx_sol = result.GetSolution(f.dx)
+u_sol = result.GetSolution(f.u)
+plt.figure()
+plt.subplot(311)
+plt.plot(time, x_sol[0,:], label='q1')
+plt.plot(time, x_sol[1,:], label='q2')
+plt.plot(time, x_sol[2,:], label='q3')
+plt.plot(time, x_sol[3,:], label='q4')
+plt.plot(time, x_sol[4,:], label='q5')
+
+plt.subplot(312)
+plt.plot(time, dx_sol[0,:], label='dq1')
+plt.plot(time, dx_sol[1,:], label='dq2')
+plt.plot(time, dx_sol[2,:], label='dq3')
+plt.plot(time, dx_sol[3,:], label='dq4')
+plt.plot(time, dx_sol[4,:], label='dq5')
+
+plt.subplot(313)
+plt.plot(time, u_sol[0,:], label='u1')
+plt.plot(time, u_sol[1,:], label='u2')
+plt.plot(time, u_sol[2,:], label='u3')
+plt.plot(time, u_sol[3,:], label='u4')
+
+# plt.xlabel('q')
+# plt.ylabel('qdot')
+plt.show()
+# solver = IpoptSolver()
+# result = solver.Solve(f.prog, None, None)
+
+# assert(result.is_success()), "Optimization failed"
+
+# print("Success? ", result.is_success())
+# # Print the solution to the decision variables.
+# # print('x* = ', result.GetSolution(x))
+# # Print the optimal cost.
+# print('optimal cost = ', result.get_optimal_cost())
+# # Print the name of the solver that was called.
+# print('solver is: ', result.get_solver_id().name())
+
+
+
+
+
+	# def ComputeTransformations(self):
+	# 	self.world_frame = np.array([0, 0]).reshape(2, 1)
+	# 	self.T01 = []; self.T12 = []; self.T23 = []; self.T24 = []; self.T45 = []
+	# 	for n in range(self.N):
+	# 		s1, c1 = np.sin(self.x[0, n]), np.cos(self.x[0, n]) 
+	# 		s2, c2 = np.sin(self.x[1, n]), np.cos(self.x[1, n]) 
+	# 		s3, c3 = np.sin(self.x[2, n]), np.cos(self.x[2, n]) 
+	# 		s4, c4 = np.sin(self.x[3, n]), np.cos(self.x[3, n]) 
+	# 		s5, c5 = np.sin(self.x[4, n]), np.cos(self.x[4, n]) 
+			
+	# 		self.T01.append(np.array([
+	# 			[c1, s1, self.left_foot[0, n]],
+	# 			[-s1, c1, self.left_foot[1, n]],
+	# 			[0, 0, 1]
+	# 		]))
+	# 		self.T12.append( self.T01[-1] @ (np.array(
+	# 										[[c2, s2, self.l[0]],
+	# 										[-s2, c2, self.l[0]],
+	# 										[0, 0, 1]]
+	# 										))
+	# 						)
+	# 		self.T23.append( self.T12[-1] @ (np.array(
+	# 										[[c3, s3, self.l[1]],
+	# 										[-s3, c3, self.l[1]],
+	# 										[0, 0, 1]]
+	# 										))
+	# 						)
+	# 		self.T24.append( self.T12[-1] @ (np.array(
+	# 										[[c4, s4, self.l[0]],
+	# 										[-s4, c4, self.l[0]],
+	# 										[0, 0, 1]]
+	# 										))
+	# 						)
+	# 		# print(self.T01)
+
+	# def Visualize(self):
+	# 	fig = plt.figure()
+	# 	time = np.linspace(0, self.T, self.N)
+	# 	ax = plt.gca()
+	# 	# ax.plot(curve_x, 9./curve_x)
+	# 	# ax.plot(-curve_x, -9./curve_x)
+	# 	# ax.plot(0, 0, 'o')
+	# 	x_init = [-0.6,0.7,0.0,-0.5,-0.3]
+	# 	point_x1, = ax.plot([], [], 'x')
+	# 	ax.axis('equal')
+
+	# 	def update(x):
+	# 		global iter_count
+	# 		point_x.set_xdata()
+	# 		point_x.set_ydata(x)
+	# 		ax.set_title(f"iteration {iter_count}")
+	# 		fig.canvas.draw()
+	# 		fig.canvas.flush_events()
+	# 		# Also update the iter_count variable in the callback.
+	# 		# This shows we can do more than just visualization in
+	# 		# callback.
+	# 		iter_count += 1
+	# 		plt.pause(0.1)
+	# 	iter_count = 0
+
+	# 	for n in range(self.N):
+	# 		self.prog.AddVisualizationCallback(update, self.x[:,n])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # def constraint_eval(z):
 #        p = np.cos(z)
 #        return (1 - forwarddiff.derivative(p, z))
