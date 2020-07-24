b58048e8a61e36cd1a0068579fccb37f42553efa
aditya-shirwatkar <cosmosaditya02@gmail.com>
2020-07-24 19:35:26 +0530

    biped test

---
+++ biped_tutorial.py
@@ -0,0 +1,16 @@
+from pydrake.symbolic import Variable
+from pydrake.systems.primitives import SymbolicVectorSystem
+import pydrake.math as pymath
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+u = np.empty((4, 1), dtype=Variable)
+x = np.empty((5, 1), dtype=Variable)
+
+for n in range(5):
+    x[n] = Variable('q' + str(n))
+    if n != 5-1:
+        u[n] = Variable('u' + str(n+1))
+
+
