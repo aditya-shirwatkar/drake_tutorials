9c6511311845c2655029a2d8514a2275d31015d8
aditya-shirwatkar <cosmosaditya02@gmail.com>
2020-07-24 19:36:17 +0530

    biped test

--- biped_tutorial.py
+++ biped_tutorial.py
@@ -7,9 +7,11 @@ import matplotlib.pyplot as plt
 
 u = np.empty((4, 1), dtype=Variable)
 x = np.empty((5, 1), dtype=Variable)
+dx = np.empty((5, 1), dtype=Variable)
 
 for n in range(5):
     x[n] = Variable('q' + str(n))
+    dx[n] = Variable('dq' + str(n))
     if n != 5-1:
         u[n] = Variable('u' + str(n+1))
 
